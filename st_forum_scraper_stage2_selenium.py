81ae9fbb6781d35947d5651b95f26f584195c8a3
David Goh <dg282@duke.edu>
2025-04-14 21:35:50 +0800

    unfortunately one needs to watch the windows open and close the ads manually if they appear

--- st_forum_scraper_stage2_selenium.py
+++ st_forum_scraper_stage2_selenium.py
@@ -28,7 +28,7 @@ def load_forum_links():
 def get_article_details(url):
     # Setup headless Chrome browser
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
 
     driver = webdriver.Chrome(options=chrome_options)
